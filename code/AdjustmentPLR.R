0ca509d411ec27a29da530e64dd385bb375b856e
João Pedro <jpedrofgomes@gmail.com>
2025-04-03 14:02:53 -0300

    fix: small change in plr file

--- code/AdjustmentPLR.R
+++ code/AdjustmentPLR.R
@@ -1,17 +1,18 @@
 options(scipen = 999)
+gc()
 library(tidyverse)
 library(this.path)
 setwd(this.dir())
-cores_made <- c("#45ff66", "#eb52ff", "#3366ff","#feff41")
 load('../data/baseRendimentosIsentos.Rda')
 
 rendaMaxima99 <- pnadc_receita_final %>% filter(Centésimo == 99) %>% pull(rendimento_todasfontes_calibrado) %>% max()
 pnadc_receita_final <- pnadc_receita_final %>% mutate(centesimoReceita = if_else(rendimento_todasfontes_calibrado>rendaMaxima99, 100, Centésimo))
+pnadc_receita_final <- pnadc_receita_final %>% mutate(`Participação nos Lucros ou Resultados` = replace_na(`Participação nos Lucros ou Resultados`,0))
 pnadc_receita_final <- pnadc_receita_final %>% group_by(centesimoReceita) %>% mutate(TotalPlr = sum(`Participação nos Lucros ou Resultados`)) %>% ungroup()
 
 metaGrupo <- 384168/8
 
-pnad_ordenada <- pnadc_receita_final %>% filter(!is.na(centesimoReceita))%>% group_by(centesimoReceita) %>%
+pnad_ordenada <- pnadc_receita_final %>% filter(!is.na(centesimoReceita)) %>% group_by(centesimoReceita) %>%
   arrange(desc(rendimento_todasfontes_calibrado), .by_group = TRUE) %>%
   mutate(
     peso_acumulado = cumsum(peso_comcalib)
@@ -25,6 +26,6 @@ pnad_ricos_centesimo <- pnad_ricos_centesimo %>% group_by(centesimoReceita) %>% 
 
 pnad_ricos_centesimo <- pnad_ricos_centesimo %>% select(c(id_ind, plr_distribuido))
 pnadc_receita_final <- pnadc_receita_final %>% left_join(pnad_ricos_centesimo, by="id_ind")
-
+pnadc_receita_final <- pnadc_receita_final %>% mutate(plr_distribuido = replace_na(plr_distribuido,0))
 save(pnadc_receita_final, file = "../data/baseRendimentosIsentosPlrAdj.Rda")
 
